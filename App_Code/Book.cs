b4663bafcc3bbbb1a491a691e851eabb7f914418
carlosdzambrano <carlosdzambrano@outlook.com>
2018-04-24 14:09:28 -0400

    Add files via upload

---
+++ App_Code/Book.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Book
+/// </summary>
+public class Book
+{
+    private string name;
+    private string author;
+    private string isbn;
+    private string genre;
+    private int    noOfPages;
+    private bool   lended;
+    private string friend;
+    private string comment;
+    
+    public Book()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+    public Book(string name, string author, string isbn, string genre, int noOfPages, bool lended, string friend, string comment)
+    {
+        this.name = name;
+        this.author = author;
+        this.isbn = isbn;
+        this.genre = genre;
+        this.noOfPages = noOfPages;
+        this.lended = lended;
+        this.friend = friend;
+        this.comment = comment;
+    }
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+    public string Author { get => author; set => author = value; }
+    public string Isbn { get => isbn; set => isbn = value; }
+    public string Genre { get => genre; set => genre = value; }
+    public int NoOfPages { get => noOfPages; set => noOfPages = value; }
+    public bool Lended { get => lended; set => lended = value; }
+    public string Friend { get => friend; set => friend = value; }
+    public string Comment { get => comment; set => comment = value; }
+
+
+}
---
+++ App_Code/BookRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for BookRepository
+/// </summary>
+public class BookRepository
+{
+    List<Book> bookList;
+    public BookRepository()
+    {
+        bookList = new List<Book>();
+    }
+
+    public List<Book> GetAllBooks()
+    {
+        return bookList;
+    }
+
+    public void AddBook(Book aBook)
+    {
+        bookList.Add(aBook);
+    }
+
+    public void RemoveBook(Book aBook)
+    {
+        bookList.Remove(aBook);
+    }
+}
